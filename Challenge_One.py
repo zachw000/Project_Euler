15bc14e72d93a772dad065365c7d69bfad6c0500
zachw000 <wewingames222@gmail.com>
2015-04-15 16:52:15 -0700

    Added Multipliers
    Not complete yet, however after this I'm going to implement
    summation so all of the multiples are added.

--- Challenge_One.py
+++ Challenge_One.py
@@ -11,6 +11,32 @@ mults = {
 # Multiplier variable
 inc = 2
 
-while mults["three"][len(mults["three"]) - 1] * (inc + 1) < 100:
+while mults["three"][len(mults["three"]) - 1] < 999:
 	mults["three"].append(mults["three"][0] * inc)
 	inc += 1
+
+# Reset incrementor
+inc = 2
+
+while mults["five"][len(mults["five"]) - 1] < 995:
+	mults["five"].append(mults["five"][0] * inc)
+	inc += 1
+
+answer = 0
+
+threes = mults["three"]
+fives = mults["five"]
+
+for i in threes:
+	answer += i
+
+#print(answer)
+
+#for i in fives:
+#	answer += i
+
+print(mults["three"])
+print()
+print(mults["five"])
+print()
+print(answer)

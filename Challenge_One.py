307b9b4a3ae56c46486429c98e11f54cf9d58e15
zachw000 <wewingames222@gmail.com>
2015-04-16 09:59:06 -0700

    Added Summations

--- Challenge_One.py
+++ Challenge_One.py
@@ -12,14 +12,14 @@ mults = {
 inc = 2
 
 while mults["three"][len(mults["three"]) - 1] < 999:
-	mults["three"].append(mults["three"][0] * inc)
+	mults["three"].append(3 * inc)
 	inc += 1
 
 # Reset incrementor
 inc = 2
 
 while mults["five"][len(mults["five"]) - 1] < 995:
-	mults["five"].append(mults["five"][0] * inc)
+	mults["five"].append(3 * inc)
 	inc += 1
 
 answer = 0
@@ -27,13 +27,10 @@ answer = 0
 threes = mults["three"]
 fives = mults["five"]
 
-for i in threes:
-	answer += i
+answer += sum([threes, fives])
 
-#print(answer)
-
-#for i in fives:
-#	answer += i
+fives = sum(fives)
+threes = sum(threes)
 
 print(mults["three"])
 print()
